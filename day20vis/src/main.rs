8d3a6896753176561f7de7058bd9162409cad806
Andy Ward <andy.ward.uk@gmail.com>
2023-12-20 11:11:52 +0000

    Day 20 vis

--- day20vis/src/main.rs
+++ day20vis/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, error::Error, fs::File, io::Write};
+use std::{error::Error, fs::File, io::Write};
 
 use aoc::input::parse_input_vec;
 
@@ -11,10 +11,77 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     file.write_fmt(format_args!("digraph {{\n"))?;
 
-    for (n, c) in components {
-        file.write_fmt(format_args!("{n} [label=\"{n}\\n{c:?}\"]\n"))?;
+    let node = |file: &mut File, name, comp: &Component| -> Result<(), Box<dyn Error>> {
+        file.write_fmt(format_args!("{name} [label=\"{name}\\n{comp:?}\""))?;
+
+        let shape = match comp {
+            Component::Conjunction => "box",
+            Component::Button => "invhouse",
+            Component::Broadcaster => "hexagon",
+            Component::FlipFlop => "oval",
+            Component::Output => "house",
+        };
+
+        file.write_fmt(format_args!(" shape=\"{shape}\""))?;
+
+        file.write_fmt(format_args!("];\n"))?;
+
+        Ok(())
+    };
+
+    // Button & broadcaster first
+    file.write_fmt(format_args!("subgraph {{\n"))?;
+
+    for (name, comp) in &components {
+        if !matches!(comp, Component::Button | Component::Broadcaster) {
+            continue;
+        }
+
+        node(&mut file, name, comp)?;
     }
 
+    file.write_fmt(format_args!("}}\n"))?;
+
+    // FlipFlops next
+    file.write_fmt(format_args!("subgraph {{\n"))?;
+
+    for (name, comp) in &components {
+        if !matches!(comp, Component::FlipFlop) {
+            continue;
+        }
+
+        node(&mut file, name, comp)?;
+    }
+
+    file.write_fmt(format_args!("}}\n"))?;
+
+    // Conjunctions next
+    file.write_fmt(format_args!("subgraph {{\n"))?;
+
+    for (name, comp) in &components {
+        if !matches!(comp, Component::Conjunction) {
+            continue;
+        }
+
+        node(&mut file, name, comp)?;
+    }
+
+    file.write_fmt(format_args!("}}\n"))?;
+
+    // Outputs
+    file.write_fmt(format_args!("subgraph {{\n"))?;
+
+    for (name, comp) in &components {
+        if !matches!(comp, Component::Output) {
+            continue;
+        }
+
+        node(&mut file, name, comp)?;
+    }
+
+    file.write_fmt(format_args!("}}\n"))?;
+
+    // Connections
     for (f, t) in connections {
         file.write_fmt(format_args!("{f} -> {t}\n"))?;
     }
@@ -24,7 +91,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 enum Component {
     Button,
     Broadcaster,
@@ -41,9 +108,12 @@ fn input_transform(line: String) -> InputEnt {
     line
 }
 
-fn parse_input(input: &[String]) -> (HashMap<String, Component>, Vec<(String, String)>) {
-    let mut components = HashMap::new();
+fn parse_input(input: &[String]) -> (Vec<(String, Component)>, Vec<(String, String)>) {
+    let mut components = Vec::new();
     let mut connections = Vec::new();
+
+    // Add button
+    components.push(("button".to_string(), Component::Button));
 
     for line in input {
         let mut split1 = line.split(" -> ");
@@ -65,24 +135,17 @@ fn parse_input(input: &[String]) -> (HashMap<String, Component>, Vec<(String, St
             .into_iter()
             .for_each(|conn| connections.push((name.to_string(), conn)));
 
-        components.insert(name.to_string(), comp);
+        components.push((name.to_string(), comp));
     }
-
-    // Add button
-    components.insert("button".to_string(), Component::Button);
 
     connections.push(("button".to_string(), "broadcaster".to_string()));
 
     // Add outputs if required
-    let missing = connections
-        .iter()
-        .filter(|(_, b)| !components.contains_key(b))
-        .map(|(a, b)| (a.to_owned(), b.to_owned()))
-        .collect::<Vec<_>>();
-
-    for (_, b) in missing {
-        components.insert(b.clone(), Component::Output);
-    }
+    connections.iter().map(|(_, b)| b).for_each(|b| {
+        if !components.iter().any(|(n, _)| n == b) {
+            components.push((b.to_string(), Component::Output))
+        }
+    });
 
     (components, connections)
 }
